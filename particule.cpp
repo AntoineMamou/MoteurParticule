5941e9ed960f021b57306e3f1c5d5b5fd46c0fea
antoineMamou <antoinemamou99@gmail.com>
2025-09-24 02:52:58 +0200

    refactoring tests for vector + new particles type

--- particule.cpp
+++ particule.cpp
@@ -1,4 +1,5 @@
 #include "particule.h"
+#include <numbers>
 
 particule::particule() {
 }
@@ -10,6 +11,7 @@ particule::particule(vect pos, vect vit) {
 	masse = 0.0f;
 	acceleration = { 0, 0, 0 };
 	damping = 0.0f;
+	trace.push_back(pos);
 }
 
 particule::particule(vect pos, vect vit, vect acc) {
@@ -19,6 +21,7 @@ particule::particule(vect pos, vect vit, vect acc) {
 	masse = 0;
 	gravity = 0;
 	acceleration = acc;
+	trace.push_back(pos);
 }
 
 vect particule::getPosition() { return position; }
@@ -36,36 +39,75 @@ void particule::draw() {
 	ofDrawCircle(glm::vec3(position.x, position.y, position.z), 5);
 }
 
+/// <summary>
+/// Function that computes accelerations based on the forces applied to the particle
+/// </summary>
+
 void particule::computeForce() {
-	//Calcul de la force de frottement
-	acceleration = vitesse * (-damping / masse);
-	//Ajout de la force de frotement (Positive car on est dans un repere retourne)
+	if (masse != 0) {
+		// Computing the friction force: friction_force = -f * v^2
+		acceleration.x = std::abs(vitesse.getNorm()) * vitesse.x * -damping / masse;
+		acceleration.y = std::abs(vitesse.getNorm()) * vitesse.y * -damping / masse;
+	}
+	// Adding the friction force (Positive since the reference frame is inverted)
 	acceleration.y += gravity;
 }
 
+/// <summary>
+///
+/// </summary>
+/// <param name="time"></param>
 void particule::EulerIntegration(float time) {
 	vect deltaVitesse = acceleration * time;
 	vitesse = vitesse + deltaVitesse;
 	vect deplacement = vitesse * time;
 
-	//On retourne la composante de vitesse verticale, car le repere de dessin est inverse
-	// 0 --> haut de l'ecran
+	/* ######## FOR DEBUG ########
+	* 
+	printf("acc (y) = %f\n", acceleration.y);
+	printf("vit (y) = %f\n\n", vitesse.y);
+	printf("deplacement (y) = %f\n\n", deplacement.y);
+
+	printf("acc (x) = %f\n", acceleration.x);
+	printf("vit (x) = %f\n", vitesse.x);
+	printf("deplacement (x) = %f\n\n", deplacement.x);
+
+	printf("angle : %f\n", std::atanf(vitesse.y / vitesse.x) * 180 / std::numbers::pi);
+
+	printf("------------------------\n");
+	*/
+
+	// Invert the vertical velocity component, since the rendering coordinate system is inverted
+	// 0 --> top of the screen
 	deplacement.y *= (-1);
 
-	//Nouvelle position = ancienne position + deplacement
+	// New position = previous position + movement
 	position = position + deplacement;
 }
 
+/// <summary>
+/// In development
+/// </summary>
+/// <param name="time"></param>
 void particule::VerletIntegration(float time) {
 	vect previous_pos = position;
 	vect temp = acceleration * time * time;
 	position = position * 2 - previous_pos + temp;
 }
 
+/// <summary>
+/// Function to modify the mass of the particle
+/// </summary>
+///
+/// <param name="m">masse de la particule</param>
 void particule::setMasse(float m) {
 	masse = m;
 }
 
+/// <summary>
+/// Function to modify the gravity applied to the particle
+/// </summary>
+/// <param name="g"></param>
 void particule::setGravity(float g) {
 	gravity = g;
 }
@@ -86,6 +128,12 @@ float particule::getDamping() {
 	return damping;
 }
 
+/// <summary>
+/// Function which return an array with all the positions all the previous positions of the particle
+/// </summary>
+/// <returns>
+/// Vector<Vect> storing the past positions
+/// </returns>
 std::vector<vect> particule::getTraceArray() {
 	return trace;
 }
@@ -93,6 +141,9 @@ void particule::addTracePosition(vect pos) {
 	trace.push_back(pos);
 }
 
+/// <summary>
+/// Function that plots a line through all the past positions
+/// </summary>
 void particule::drawTrace() {
 	if (trace.size() >= 2) {
 		for (int i = 1; i < trace.size(); i++) {
@@ -101,6 +152,12 @@ void particule::drawTrace() {
 	}
 }
 
+/// <summary>
+/// Allows drawing the trail of an object by specifying the trail color
+/// </summary>
+/// <param name="color">
+/// Trail color in the format: ofColor(R,G,B)
+/// </param>
 void particule::drawTrace(ofColor color) {
 	ofSetColor(color);
 	drawTrace();
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,45 +1,32 @@
 #include "ofApp.h"
 #include <numbers>
-#include "../testVector.h"
+#include "..//testVector.h";
 
-std::vector<particule> particuleArray; // Vector containing all the particle
-float angle = 45; // Base angle
+std::vector<particule> particuleArray;
+float angle = 30;
 float duration = 0;
-float gravity = -9.81;
 
 //--------------------------------------------------------------
 void ofApp::setup() {
-
-	//run the tests for the Vector class
-	TestVector::runTests();
-
-	// window setup
 	ofSetVerticalSync(true);
 	ofBackground(0, 0, 0, 0);
 
-	vect v1 = { 1, 0, 0 };
-	vect v2 = { 0, 1, 0 };
-
-	int a = v1.ProduitScalaire(v2);
-	vect a1 = v1.ProduitVectoriel(v2);
+	TestVector::runTests();
 }
 
 //--------------------------------------------------------------
-// for each particle apply Force, calculate EulerIntegration, add trace position
 void ofApp::update() {
 	for (auto & part : particuleArray) {
 		if (part.getPosition().y <= 600) {
 			float time = 1 / ofGetFrameRate();
 			part.computeForce();
-			part.EulerIntegration(time); // Calculate next position
-			part.addTracePosition(part.getPosition()); // Track position
+			part.EulerIntegration(time);
+			part.addTracePosition(part.getPosition());
 		}
 	}
 }
 
 //--------------------------------------------------------------
-// Draw each particle
-// Draw HUD
 void ofApp::draw() {
 	for (auto & part : particuleArray) {
 		part.drawTrace(ofColor(127, 0, 0));
@@ -51,17 +38,67 @@ void ofApp::draw() {
 
 	duration += 1 / ofGetFrameRate();
 	char msgTime[128];
-	sprintf(msgTime, "Duree de la simulation : %.2f (s)", duration);
+	sprintf(msgTime, "Durée de la simulation : %.2f (s)", duration);
 	ofDrawBitmapStringHighlight(msgTime, 100, 100);
 
 	char msgAngle[128];
-	sprintf(msgAngle, "Angle : %.f ", angle);
+	sprintf(msgAngle, "Angle : %.f °", angle);
 	ofDrawBitmapStringHighlight(msgAngle, 100, 120);
+
+	char cmd[256];
+	sprintf(cmd, "Commandes :\n\nLancer boule de feu : 'f'\nLancer balle : 'b'\nTirer laser : 'l'\nLancer boule de canon : 'c'\n\nModifier l'angle de tir : flèche directionnelle up/dwn\n\nReset l'affichage : backspace");
+	ofDrawBitmapStringHighlight(cmd, 600, 100);
 }
 
 //--------------------------------------------------------------
-// Modify angle shoot
 void ofApp::keyPressed(int key) {
+	if (key == 112) {
+		particule p1({ ofRandom(ofGetWindowSize().x), ofRandom(ofGetWindowSize().y), 0 }, { ofRandom(100), ofRandom(100), 0 });
+		p1.setDamping(0.01);
+		p1.setMasse(12);
+		p1.setGravity(-9.81);
+		particuleArray.push_back(p1);
+	}
+	if (key == 'c') {
+		float speed = 150; // vitesse initiale de 150m/s
+		particule canon({ 100, 550, 0 }, { speed * std::cosf(angle / 360 * 2 * std::numbers::pi), speed * std::sinf(angle / 360 * 2 * std::numbers::pi), 0 }, { 0, 0, 0 });
+		canon.setDamping(0.03);
+		canon.setMasse(12);
+		canon.setGravity(-9.81);
+		particuleArray.push_back(canon);
+	}
+
+	// send a laser beam
+	if (key == 'l') {
+		float speed = 5000;
+		particule laser({ 100, 550, 0 }, { speed * std::cosf(angle / 360 * 2 * std::numbers::pi), speed * std::sinf(angle / 360 * 2 * std::numbers::pi), 0 }, { 0, 0, 0 });
+		laser.setDamping(0.0f);
+		laser.setMasse(0.0f);
+		laser.setGravity(-9.81);
+		particuleArray.push_back(laser);
+	}
+
+	if (key == 'f') {
+		float speed = 50;
+		particule fireball({ 100, 550, 0 }, { speed * std::cosf(angle / 360 * 2 * std::numbers::pi), speed * std::sinf(angle / 360 * 2 * std::numbers::pi), 0 }, { 0, 0, 0 });
+		fireball.setDamping(0.0f);
+		fireball.setMasse(0.0f);
+		fireball.setGravity(0);
+		particuleArray.push_back(fireball);
+	}
+
+	if (key == 'b') {
+		float speed = 500;
+		particule bullet({ 100, 550, 0 }, { speed * std::cosf(angle / 360 * 2 * std::numbers::pi), speed * std::sinf(angle / 360 * 2 * std::numbers::pi), 0 }, { 0, 0, 0 });
+		bullet.setDamping(0.0001f);
+		bullet.setMasse(0.01f);
+		bullet.setGravity(-9.81f);
+		particuleArray.push_back(bullet);
+	}
+
+	if (key == 8) {
+		particuleArray.clear();
+	}
 	// Arrow down
 	if (key == 57359) {
 		angle -= 1;
@@ -74,37 +111,7 @@ void ofApp::keyPressed(int key) {
 }
 
 //--------------------------------------------------------------
-// p - Shoot canon bullet from a random place
-// c - Shoot canon bullet from an origin point
-// l - Shoot laser 
 void ofApp::keyReleased(int key) {
-	if (key == 'p') {
-		particule p1({ ofRandom(ofGetWindowSize().x), ofRandom(ofGetWindowSize().y), 0 }, { ofRandom(100), ofRandom(100), 0 });
-		p1.setDamping(0.01);
-		p1.setMasse(12);
-		p1.setGravity(gravity);
-		particuleArray.push_back(p1);
-	}
-	if (key == 'c') {
-		float speed = 100; // vitesse initiale de 100m/s
-		particule canon({ 100, 600, 0 }, { speed * std::cosf(angle / 360 * 2 * std::numbers::pi), speed * std::sinf(angle / 360 * 2 * std::numbers::pi), 0 }, { 0, 0, 0 });
-		canon.setDamping(1.5);
-		canon.setMasse(12);
-		canon.setGravity(gravity);
-		particuleArray.push_back(canon);
-	}
-	// send a laser beam
-	if (key == 'l') {
-		float speed = 500;
-		particule laser({ 100, 600, 0 }, { speed * std::cosf(angle / 360 * 2 * std::numbers::pi), speed * std::sinf(angle / 360 * 2 * std::numbers::pi), 0 }, { 0, 0, 0 });
-		laser.setDamping(0.000);
-		laser.setMasse(0.0001f);
-		laser.setGravity(gravity);
-		particuleArray.push_back(laser);
-	}
-
-
-
 }
 
 //--------------------------------------------------------------
